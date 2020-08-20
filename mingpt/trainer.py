2c9feb6d0ea4b8fd7299b2c82a1c700e9ecd7488
Michael Lavelle <michael@lavelle.name>
2020-08-20 09:44:24 +0100

    Fixing HuggingFace LayerNorm.weight reference - replacing with PyTorch equivalents

--- mingpt/trainer.py
+++ mingpt/trainer.py
@@ -60,7 +60,7 @@ class Trainer:
         model, config = self.model, self.config
 
         # create the optimizer
-        no_decay = ["bias", "LayerNorm.weight"]
+        no_decay = ["bias", "ln1.weight", "ln2.weight"]
         params_decay = [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)]
         params_nodecay = [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)]
         optim_groups = [
