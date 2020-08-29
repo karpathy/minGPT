08f5b9ac03633121c33b0f11e5b5acdca61b665c
Andrej Karpathy <andrej.karpathy@gmail.com>
2020-08-29 12:28:49 -0700

    refactor out the learning rate decay class as a callback

--- mingpt/trainer.py
+++ mingpt/trainer.py
@@ -24,10 +24,6 @@ class TrainerConfig:
     betas = (0.9, 0.95)
     grad_norm_clip = 1.0
     weight_decay = 0.1 # only applied on matmul weights
-    # learning rate decay params: linear warmup followed by cosine decay to 10% of original
-    lr_decay = False
-    warmup_tokens = 375e6 # these two numbers come from the GPT-3 paper, but may not be good defaults elsewhere
-    final_tokens = 260e9 # (at what point we reach 10% of original LR)
     # checkpoint settings
     ckpt_path = None
     num_workers = 0 # for DataLoader
@@ -36,13 +32,45 @@ class TrainerConfig:
         for k,v in kwargs.items():
             setattr(self, k, v)
 
+class WarmupCosineLearningRateDecay:
+    """
+    based on the number of tokens seen during training will adjust the learning rate:
+    1. first it will start at zero and gradually ramp up to full learning rate
+    2. then it will decay down with the cosine learning rate decay down until 10% of original
+    """
+
+    def __init__(self, learning_rate, warmup_tokens, final_tokens):
+        super().__init__()
+        self.learning_rate = learning_rate
+        self.warmup_tokens = warmup_tokens
+        self.final_tokens = final_tokens
+        # state in this class, will count number of tokens processed so far
+        self.tokens = 0
+
+    def on_train_batch_end(self, trainer, pl_module, batch, batch_idx=None, dataloader_idx=None):
+        _, y = batch
+        self.tokens += (y >= 0).sum()  # y == -100 is "ignore", so don't count these
+        if self.tokens < self.warmup_tokens:
+            # linear warmup
+            lr_mult = float(self.tokens) / float(max(1, self.warmup_tokens))
+        else:
+            # followed by cosine learning rate decay
+            progress = float(self.tokens - self.warmup_tokens) / float(
+                max(1, self.final_tokens - self.warmup_tokens))
+            lr_mult = 0.1 + 0.5 * (1.0 + math.cos(math.pi * progress))
+        lr = self.learning_rate * lr_mult
+        for optimizer in trainer.optimizers:
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
+
 class Trainer:
 
-    def __init__(self, model, train_dataset, test_dataset, config):
+    def __init__(self, model, train_dataset, test_dataset, config, callbacks=None):
         self.model = model
         self.train_dataset = train_dataset
         self.test_dataset = test_dataset
         self.config = config
+        self.callbacks = [] if callbacks is None else callbacks
 
         # take over whatever gpus are on the system
         self.device = 'cpu'
@@ -60,6 +88,7 @@ class Trainer:
         model, config = self.model, self.config
         raw_model = model.module if hasattr(self.model, "module") else model
         optimizer = raw_model.configure_optimizers()
+        self.optimizers = [optimizer]
 
         def run_epoch(split):
             is_train = split == 'train'
@@ -91,23 +120,13 @@ class Trainer:
                     torch.nn.utils.clip_grad_norm_(model.parameters(), config.grad_norm_clip)
                     optimizer.step()
 
-                    # decay the learning rate based on our progress
-                    if config.lr_decay:
-                        self.tokens += (y >= 0).sum() # number of tokens processed this step (i.e. label is not -100)
-                        if self.tokens < config.warmup_tokens:
-                            # linear warmup
-                            lr_mult = float(self.tokens) / float(max(1, config.warmup_tokens))
-                        else:
-                            # cosine learning rate decay
-                            progress = float(self.tokens - config.warmup_tokens) / float(max(1, config.final_tokens - config.warmup_tokens))
-                            lr_mult = max(0.1, 0.5 * (1.0 + math.cos(math.pi * progress)))
-                        lr = config.learning_rate * lr_mult
-                        for param_group in optimizer.param_groups:
-                            param_group['lr'] = lr
-                    else:
-                        lr = config.learning_rate
+                    # notify all relevant callbacks that a batch update ended. e.g. a callback may decay learning rate
+                    for cb in self.callbacks:
+                        if hasattr(cb, 'on_train_batch_end'):
+                            cb.on_train_batch_end(self, None, (x, y))
 
                     # report progress
+                    lr = optimizer.param_groups[0]['lr']
                     pbar.set_description(f"epoch {epoch+1} iter {it}: train loss {loss.item():.5f}. lr {lr:e}")
 
             if not is_train:
@@ -116,7 +135,6 @@ class Trainer:
                 return test_loss
 
         best_loss = float('inf')
-        self.tokens = 0 # counter used for learning rate decay
         for epoch in range(config.max_epochs):
 
             run_epoch('train')
