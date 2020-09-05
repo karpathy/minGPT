686b41c0f88004f7dd1e00548d4ae71b1e74f6cd
Happy Sugar Life <777Jonathansum@gmail.com>
2020-09-05 02:07:22 -1200

    [Racially Neutral Code]
    [Racially Neutral Code]
    Changing:
    blacklist/whitelist -> blocklist/passlist

--- mingpt/model.py
+++ mingpt/model.py
@@ -141,9 +141,9 @@ class GPT(nn.Module):
 
         # separate out all parameters to those that will and won't experience regularizing weight decay
         decay = set()
-        no_decay = set()
-        whitelist_weight_modules = (torch.nn.Linear, )
-        blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
+        no_decay = set() 
+        passlist_weight_modules = (torch.nn.Linear, )
+        blocklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
         for mn, m in self.named_modules():
             for pn, p in m.named_parameters():
                 fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
@@ -151,11 +151,11 @@ class GPT(nn.Module):
                 if pn.endswith('bias'):
                     # all biases will not be decayed
                     no_decay.add(fpn)
-                elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
-                    # weights of whitelist modules will be weight decayed
+                elif pn.endswith('weight') and isinstance(m, passlist_weight_modules):
+                    # weights of passlist modules will be weight decayed
                     decay.add(fpn)
-                elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
-                    # weights of blacklist modules will NOT be weight decayed
+                elif pn.endswith('weight') and isinstance(m, blocklist_weight_modules):
+                    # weights of blocklist modules will NOT be weight decayed
                     no_decay.add(fpn)
 
         # special case the position embedding parameter in the root GPT module as not decayed
